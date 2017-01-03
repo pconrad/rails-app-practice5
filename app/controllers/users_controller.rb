e64b4db6f7928a27cb2cf096df5866df22c0b9e0
Phill Conrad <pconrad@cs.ucsb.edu>
2017-01-03 15:30:18 -0800

    testing membership

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
+    Octokit.auto_paginate = true
+    
     @octokit_client = Octokit::Client.new(:access_token => session[:oauth_token])
     @github_user = @octokit_client.user
     @emails = @octokit_client.emails
+    @user_authenticated = @octokit_client.user_authenticated?
+
+    @login = @octokit_client.user.login
+    @is_member_f16 = @octokit_client.organization_member?('UCSB-CS56-F16',@login)
+    @is_member_w15 = @octokit_client.organization_member?('UCSB-CS56-W15',@login)
     @organizations = @octokit_client.organizations
-    
+        
   end
 
 end
